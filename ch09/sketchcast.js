6f5092491c5251d14311adb7d2ad258f9b07f9eb
Jack Moffitt <jack@metajack.im>
2011-03-01 10:41:22 -0700

    Changed localhost URLs to bosh.metajack.im.

--- ch09/sketchcast.js
+++ ch09/sketchcast.js
@@ -271,7 +271,7 @@ $(document).bind('connect', function (ev, data) {
     $('#status').html('Connecting...');
     
     var conn = new Strophe.Connection(
-        'http://localhost:5280/xmpp-httpbind');
+        'http://bosh.metajack.im:5280/xmpp-httpbind');
 
     conn.connect(data.jid, data.password, function (status) {
         if (status === Strophe.Status.CONNECTED) {
