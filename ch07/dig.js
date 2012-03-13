3735b352af090f8ba68cb888926a5c6efde4093a
Jack Moffitt <jack@metajack.im>
2012-03-12 22:30:24 -0600

    Fix missing semicolon.

--- ch07/dig.js
+++ ch07/dig.js
@@ -145,7 +145,7 @@ $(document).ready(function () {
 
 $(document).bind('connect', function (ev, data) {
     var conn = new Strophe.Connection(
-        'http://bosh.metajack.im:5280/xmpp-httpbind')
+        'http://bosh.metajack.im:5280/xmpp-httpbind');
 
     conn.connect(data.jid, data.password, function (status) {
         if (status === Strophe.Status.CONNECTED) {
